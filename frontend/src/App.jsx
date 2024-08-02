7a610551efd7d41ea4117769692ee3e4c64aa90a
MuhammadSalmann <salmansidd372@gmail.com>
2024-08-02 20:04:30 +0500

    Added react router dom, Link and Link Container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -8,7 +9,8 @@ export const App = () => {
       <Header />
       <main className='py-3'>
           <Container>
-          <h1>Welcome to ProShop</h1>
+          <h1>Welcome to ProTech</h1>
+          <Outlet />
           </Container>
       </main>
       <Footer />
