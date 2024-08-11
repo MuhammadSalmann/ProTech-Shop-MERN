d857e584d3cfe10175a74ebd9be9e3bc0beb1913
MuhammadSalmann <salmansidd372@gmail.com>
2024-08-11 07:23:42 +0500

    Rating and ProductScreen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
-} from 'react-router-dom'
 import { App } from './App.jsx'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css'
-import HomeScreen from './screens/HomeScreen.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css'  // This is the default bootstrap css
+import './assets/styles/bootstrap.custom.css'  // This is the custom bootstrap css
+import './assets/styles/index.css'  // This is the custom css
 
-const router = createBrowserRouter(
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+import HomeScreen from './screens/HomeScreen.jsx'
+import ProductScreen from './screens/ProductScreen.jsx'
+
+const router = createBrowserRouter( 
   createRoutesFromElements(
     <Route path='/' element={<App />} >
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
-
   )
 )
 
