951baa2a1ed18ca5c7197e1f02449b84d22e83ea
MuhammadSalmann <salmansidd372@gmail.com>
2024-09-01 07:10:38 +0500

    vercel.json and changes in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // default, you can specify a different directory here if needed
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
